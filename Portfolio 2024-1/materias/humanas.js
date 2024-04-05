9fd3ceb97190c9480b094ecb729d7ae7fabdc689
EduardoKIB <128161229+EduardoKIB@users.noreply.github.com>
2024-04-04 23:51:27 -0300

    Add files via upload

---
+++ Portfolio 2024-1/materias/humanas.js
@@ -0,0 +1,14 @@
+document.getElementById('btnPrimeiro').addEventListener('click', function() {
+    document.getElementById('assignments').style.display = 'block';
+    document.getElementById('mensagem').style.display = 'none';
+});
+
+document.getElementById('btnSegundo').addEventListener('click', function() {
+    document.getElementById('assignments').style.display = 'none';
+    document.getElementById('mensagem').style.display = 'block';
+});
+
+document.getElementById('btnTerceiro').addEventListener('click', function() {
+    document.getElementById('assignments').style.display = 'none';
+    document.getElementById('mensagem').style.display = 'block';
+});
